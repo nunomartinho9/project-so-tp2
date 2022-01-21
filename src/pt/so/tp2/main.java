016349e8d1e5df264f5b3cd92c47f5378a7dfc80
Samuel Ribeiro <samuelferreira2001@gmail.com>
2022-01-21 18:04:54 +0000

    Cost and chenanigans

--- src/pt/so/tp2/main.java
+++ src/pt/so/tp2/main.java
@@ -4,11 +4,9 @@ package pt.so.tp2;
 public class main {
     public static void main(String[] args) throws InterruptedException {
         Params p = Params.readFile("pcu_tests/prob04.txt");
-<<<<<<< HEAD
-        System.out.println(new GenerateSolution(p));
-=======
+
         System.out.println(new Solution(p));
->>>>>>> main
+
 
 
     }

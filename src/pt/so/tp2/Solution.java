e06ef71a9cb87b460b6bf7fc5884dc9f4ecb68eb
Tiago <tiagobatista@u-factor.io>
2022-01-22 01:32:58 +0000

    chooseP(wrong), mutate(unfinished)

--- src/pt/so/tp2/Solution.java
+++ src/pt/so/tp2/Solution.java
@@ -18,6 +18,7 @@ public class Solution {
         //fillSolutionVector();
         calcWaste(p);
         cost = calcCost();
+        mutate();
     }
 
     private void fillVector(Integer...i){
@@ -70,21 +71,78 @@ public class Solution {
         float sum = 0;
         for (Integer i : hasWaste) {
             sum += i.floatValue()/p.m;
-            System.out.println(sum);
+            //System.out.println(sum);
         }
         return sum;
     }
+
     public float calcCost() {
         return 1/((float)waste.size()+1)*(calcSum1()+calcSum2());
     }
+
+    //Funcao para escolher a placa baseado na probabilidade
+    private int chooseP(float r, LinkedList<Float>probs){
+        float probA = 0;
+        for(int i = 0; i<probs.size();i++){
+            if(r <= probA+probs.get(i) && r > probA){
+                return i;
+            }
+            probA += probs.get(i);
+        }
+        return probs.size();
+    }
+
+    //Funcao que implementa a Mutaçao 3ps
+    public void mutate(){
+        Random rng = new Random();
+        int max = waste.size();
+        int p1, p2, p3;
+        float sumDeBaixo = 0;
+        float r = rng.nextFloat();
+        LinkedList<Float> probs = new LinkedList<>();
+
+        for (Integer i : waste) {
+            if(i!=0)sumDeBaixo += Math.sqrt(1/i.floatValue());
+        }
+
+        for(Integer w : waste){
+            float prob;
+            if(w!=0){
+                prob = ((float) Math.sqrt(1/w.floatValue()))/sumDeBaixo;
+                probs.add(prob);
+            }
+            else{
+                probs.add((float) 0);
+            }
+        }
+
+        p1 = rng.nextInt(max);          //Gera p1 com uma placa randomizada
+        p2 = chooseP(r, probs);         //Gera p2 com a funçao
+        while(p2==p1){
+            p2 = chooseP(r, probs);     //Enquanto p2 for igual a p1
+        }
+        r = rng.nextFloat();            //Dá reroll do r
+        p3 = chooseP(r, probs);         //Gera p3 com a funçao e com o novo r
+        while(p3==p1 || p3==p2){
+            p3 = chooseP(r, probs);     //Enquanto p3 for igual a p2 ou p1
+        }
+
+        //Testes
+        ArrayList<Integer> pila = new ArrayList<>();
+        pila.add(p1);
+        pila.add(p2);
+        pila.add(p3);
+        System.out.println("Pila: " + pila);
+    }
+
     @Override
     public String toString() {
         return "GenerateSolution{" +
-                "p=" + p +
-                ", vetor=" + vector +
+                "Params=" + p +
+                ", vector=" + vector +
                 ", waste=" + waste +
                 ", hasWaste=" + hasWaste +
-                ", count=" + endOfPattern +
+                ", endOfPattenr=" + endOfPattern +
                 ", cost=" + cost +
                 '}';
     }
